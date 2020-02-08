8193cc2fbf8528a82d4f50993eb22e133b697197
Tyrone Damasceno <tyronedamasceno@gmail.com>
2020-02-08 09:42:37 -0300

    Modify id field to use UUID

--- api/models.py
+++ api/models.py
@@ -1,8 +1,11 @@
+import uuid
+
 from django.db import models
 
 from api.enum import ChessPieceColor, ChessPieceType
 
 
 class ChessPiece(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     type = models.CharField(max_length=20, choices=ChessPieceType.choices())
     color = models.CharField(max_length=20, choices=ChessPieceColor.choices())
