68915333798e0929498f17c0f12756b848d900a7
Tyrone Damasceno <tyronedamasceno@gmail.com>
2020-02-09 21:08:27 -0300

    Setup docker and docker-compose

--- chessboard/settings.py
+++ chessboard/settings.py
@@ -80,11 +80,13 @@ WSGI_APPLICATION = 'chessboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
